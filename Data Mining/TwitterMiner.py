cd75bbc43fcef7163a9757ad6211e085cd51563c
Maksymilian5275 <44290348+Maksymilian5275@users.noreply.github.com>
2021-01-13 18:11:23 +0000

    Update TwitterMiner.py

--- Data Mining/TwitterMiner.py
+++ Data Mining/TwitterMiner.py
@@ -1,10 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = '0QVZp8SyuB46PieJH5o8En5Xj'
-consumer_secret = 'NhXvOGa9k6nm34FMdUzfUTu0tdMM10VzfybQ99QCsQJ7Sz0N3W'
-access_token = '67682537-3APG0D5HbVOB2qJ0IoJVrtqCfE8GCM3owYBpZ5o9L'
-access_secret = 'LPM6dLg6h9gQy8Mg4lQRM96aDoLAzjSq88G8ZxeLkNlvZ'
+consumer_key = #
+consumer_secret = #
+access_token = #
+access_secret = #
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
